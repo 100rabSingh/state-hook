1c705d5babaf8cba2b4bd320082886bdcc88a608
100rabSingh <100rab2000@gmail.com>
2021-12-14 23:51:00 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -10,16 +10,19 @@ import HookCounterFour from './components/HookCounterFour';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
 import IntervalHookCounter from './components/IntervalHookCounter';
+import ReducerCounterOne from './components/ReducerCounterOne';
 import { UserProvider, ChannelProvider } from './components/UserContext';
 
 function App() {
   return (
     <div className="App">
-      <UserProvider value='100rab'>
+
+      <ReducerCounterOne />
+      {/* <UserProvider value='100rab'>
         <ChannelProvider value='ReactPractice'>
           <ContextComponentC />
         </ChannelProvider>
-      </UserProvider>
+      </UserProvider> */}
 
       {/* <EffectDataFetching /> */}
       {/* <IntervalHookCounter /> */}
